4d59672be90dc7e9f7b5c4f4327a0b213a839fee
Ford Powers <fordepowers@gmail.com>
2019-12-08 13:34:20 -0800

    Update Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Nav, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import picture from './tanner.jpg';
 import NavbarCustom from '../NavbarCustom/NavbarCustom';
 
 class Home extends React.Component {
@@ -15,23 +16,24 @@ class Home extends React.Component {
     return (
       <div className='Home'>
         <NavbarCustom activeTab='/home' />
-        {/* <Carousel {...carouselProps} /> */}
-        <Card className='bg-dark text-white' style={{ marginTop: '64px' }}>
-          <Card.Img src='https://www.contractormag.com/sites/contractormag.com/files/styles/article_featured_retina/public/Cybersecurity2.jpg?itok=Djctp7JP' alt='Card image' />
+        <Card className='bg-dark text-white' style={{ marginTop: '64px', height: '80vh' }}>
+          <Card.Img src={picture} alt='Card image' />
           <Card.ImgOverlay>
-            <Card.Title><h1 className='welcome'>Columbia Basin College Cyberhawks</h1></Card.Title>
-            <Card.Text>
-              <p>
-                An elite cybersecurity team, we come from a small school but have big ambitions.
-                <br />
-                We regularly <Link to='/awards' className='in-text-link'>attend and place</Link> at nationally ranked cybersecurity competitions.
-                <br />
-                <b>Interested?</b> Click the button below.
-              </p>
-              <p>
-                <Nav.Link target='_blank' href='https://discord.gg/XBgxUnc'><Button size='lg' variant='dark'>Join the Discord</Button></Nav.Link>
-              </p>
-            </Card.Text>
+            <div>
+              <Card.Title><h1 className='welcome'>Columbia Basin College Cyberhawks</h1></Card.Title>
+              <Card.Text>
+                <p className='welcome-text'>
+                  An elite cybersecurity club, we come from a small school but have big ambitions.
+                  <br />
+                  We regularly <Link to='/awards' className='in-text-link'>attend and place</Link> at nationally ranked cybersecurity competitions.
+                  <br />
+                  <b>Interested?</b> Click the button below.
+                </p>
+                <p>
+                  <Nav.Link target='_blank' href='https://discord.gg/XBgxUnc'><Button size='lg' variant='dark'>Join the Discord</Button></Nav.Link>
+                </p>
+              </Card.Text>
+            </div>
           </Card.ImgOverlay>
         </Card>
       </div>
